9e93e01d89ed24b7286772cda26814dd1cc9066e
kodigo-denny <denny.herrera@kodigo.org>
2022-10-26 21:52:57 -0600

    Cambios 26/10/2022

---
+++ ejemploreact01/src/Titulo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Titulo.css';
+
+export const Titulo = () => {
+  return (
+    <div>
+        <h1>Hola mundo</h1>
+        <button className='btn btn-primary'>Click aqui</button>
+    </div>
+  )
+}
+
+
+export default Titulo;
